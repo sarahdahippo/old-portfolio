9a52900ccb0d2f00439fa800dda2e691424a085c
Sarah Cheah <scheah3@gatech.edu>
2020-08-17 17:15:21 +0800

    Added portfolio components, test assets

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const navBar = document.querySelectorAll("#nav a");
-
-window.onscroll = function () {
-  myFunction();
-};
-
+const project1Modal = document.getElementById("project1Modal");
+const project1Btn = document.getElementById("project1Btn");
+const closeModal = document.getElementsByClassName("close")[0];
 var prevLinkId = "home__link";
 var prevMoonId = "home__moon";
 
-function myFunction() {
+window.onscroll = function () {
+  activeScroll();
+};
+
+function activeScroll() {
   var dist = document.documentElement.scrollTop;
   var size = window.innerHeight;
   //console.log(dist, size)
@@ -15,11 +17,17 @@ function myFunction() {
     currLinkId = "home__link";
     currMoonId = "home__moon";
   } else if (dist < size * 2) {
-    currLinkId = "supplies__link";
-    currMoonId = "supplies__moon";
-  } else if (dist < size * 4) {
-    currLinkId = "themes__link";
-    currMoonId = "themes__moon";
+    currLinkId = "about__link";
+    currMoonId = "about__moon";
+  } else if (dist < size * 3) {
+    currLinkId = "projects__link";
+    currMoonId = "projects__moon";
+  } else if (dist < size * 5) {
+    currLinkId = "design__link";
+    currMoonId = "design__moon";
+  } else if (dist < size * 8) {
+    currLinkId = "art__link";
+    currMoonId = "art__moon";
   }
   if (prevMoonId != currMoonId) {
     replaceClass(prevMoonId, "nav__moon-active", "nav__moon-hidden");
@@ -37,4 +45,18 @@ function replaceClass(id, oldClass, newClass) {
   var elem = document.getElementById(id);
   elem.classList.remove(oldClass);
   elem.classList.add(newClass);
+}
+
+project1Btn.onclick = function() {
+	project1Modal.style.display = "block";
+}
+
+closeModal.onclick = function() {
+	project1Modal.style.display = "none";
+}
+
+window.onclick = function(e) {
+	if (e.target === project1Modal) {
+		project1Modal.style.display = "none";
+	}
 }
