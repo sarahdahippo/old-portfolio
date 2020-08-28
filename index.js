1523ba9c146c4e154c72da3e9aaffa8e93c29244
Sarah Cheah <scheah3@gatech.edu>
2020-08-28 21:02:53 +0800

    Added typewriter effect

--- index.js
+++ index.js
@@ -56,34 +56,3 @@ window.onclick = function(e) {
 		project1Modal.style.display = "none";
 	}
 }
-/*function activeScroll() {
-  var dist = document.documentElement.scrollTop;
-  var size = window.innerHeight;
-  //console.log(dist, size)
-  if (dist < size) {
-    currLinkId = "home__link";
-    currMoonId = "home__moon";
-  } else if (dist < size * 2) {
-    currLinkId = "about__link";
-    currMoonId = "about__moon";
-  } else if (dist < size * 3) {
-    currLinkId = "projects__link";
-    currMoonId = "projects__moon";
-  } else if (dist < size * 5) {
-    currLinkId = "design__link";
-    currMoonId = "design__moon";
-  } else if (dist < size * 12) {
-    currLinkId = "art__link";
-    currMoonId = "art__moon";
-  }
-  if (prevMoonId != currMoonId) {
-    replaceClass(prevMoonId, "nav__moon-active", "nav__moon-hidden");
-  }
-  if (prevLinkId != currLinkId) {
-    replaceClass(prevLinkId, "nav__link-active", "nav__link");
-  }
-  replaceClass(currLinkId, "nav__link", "nav__link-active");
-  replaceClass(currMoonId, "nav__moon-hidden", "nav__moon-active");
-  prevMoonId = currMoonId;
-  prevLinkId = currLinkId;
-}*/
