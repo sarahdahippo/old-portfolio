0d1668ac62fcb99bd5d8eb24b145e49e866ac686
Sarah Cheah <scheah3@gatech.edu>
2020-09-15 11:34:53 +0800

    Updated images

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ function activeScroll() {
   prevLinkId = currLinkId;
 }
 
+function hamburgerNav() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 function replaceClass(id, oldClass, newClass) {
   var elem = document.getElementById(id);
   elem.classList.remove(oldClass);
